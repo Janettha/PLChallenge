683058ba7f16f460598e2923dca4c88bb8e4bd88
JANETH SALAS GOMEZ <janethsalasgomez@gmail.com>
2025-06-04 16:24:05 -0600

    - Fix screen rotation for showing component battery centered with size 210x60 DP and, add fillMaxSize to the entire component

--- app/src/main/java/com/janettha/plchallenge/batteryIndicatorUI/BatteryIndicatorUI.kt
+++ app/src/main/java/com/janettha/plchallenge/batteryIndicatorUI/BatteryIndicatorUI.kt
@@ -7,15 +7,15 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
@@ -91,14 +91,13 @@ fun BatteryIndicatorUI2(
 
     Row(modifier = modifier
         .fillMaxSize()
-        .fillMaxWidth()
         .background(Color(IconsAndColors.colorBatteryBackground))
         .padding(vertical = 16.dp, horizontal = 30.dp),
-        verticalAlignment = Alignment.CenterVertically
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
     ) {
         // region Heart
         Box (modifier = Modifier
-            .weight(.2f)
             .width(48.dp)
             .height(48.dp)
             .background(
@@ -108,14 +107,14 @@ fun BatteryIndicatorUI2(
             contentAlignment = Alignment.Center
         ){
             when(batteryStatus) {
-                1, 2 -> {
+                1 -> {
                     Icon(
                         imageVector = ImageVector.vectorResource(R.drawable.ic_battery_heart_inactive),
                         tint = Color(IconsAndColors.colorBatteryInactive),
                         contentDescription = null
                     )
                 }
-                0 -> {
+                0, 2 -> {
                     Icon(
                         painter = painterResource(id = IconsAndColors.icons[currentIconIndex]),
                         contentDescription = null,
@@ -346,9 +345,7 @@ fun BatteryIndicatorUI2(
         }
         // + Pila
         Box(modifier = Modifier
-            .weight(.05f)
-            .fillMaxHeight()
-            .padding(end = 16.dp)
+            .wrapContentSize()
         ){
             Box(modifier = Modifier
                 .width(5.dp)
@@ -366,13 +363,13 @@ fun BatteryIndicatorUI2(
         // endregion
         // region Trebol
         Box(modifier = Modifier
-            .fillMaxSize()
-            .weight(.2f)
+            .wrapContentSize()
             .padding(start = 16.dp)
             .background(Color.Transparent),
             contentAlignment = Alignment.Center
         ){
             Icon(
+                modifier = Modifier.width(48.dp).height(48.dp),
                 imageVector = ImageVector.vectorResource(
                     if (batteryStatus == 2) R.drawable.ic_battery_clover_active
                     else R.drawable.ic_battery_clover_inactive
