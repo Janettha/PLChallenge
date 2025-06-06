d237996bc87317dac86a8f90236bd2ccc43a929e
JANETH SALAS GOMEZ <janethsalasgomez@gmail.com>
2025-06-06 14:59:15 -0600

    - README.md

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # PLChallenge
+# Scenario
+You're working on a rewards-based mobile application encouraging users to complete challenges and earn prizes. You've been tasked with doing R&D (Research & Development) on a new feature request from the client that introduces a celebratory confetti animation when users receive an SMS confirming their reward.
+
+# Feature Goal
+* Show confetti animation whenever a user receives an SMS from a mobile number "+4444555551" and it matches this template: "Congratulations! You've earned 500 points.".
+
+# Requirements
+* Detect incoming SMS messages
+* Filter SMS messages based on sender and specific keywords
+* Trigger confetti animation in the app when a valid SMS is received
+* The clients have shared a GIF of a basic confetti animation they would like to see replicated as closely as possible. You can find the GIF in the attachments section 👉🏼
+    - Each piece of confetti is one of the basic 2D shapes that differ in scale, rotation, and color.
+* Edge cases that don't need to be considered at this stage of development
+    - SMS received when app is closed
+    - Multiple SMS messages received in a short time
--- app/src/main/java/com/janettha/plchallenge/MainActivity.kt
+++ app/src/main/java/com/janettha/plchallenge/MainActivity.kt
@@ -1,26 +1,25 @@
 package com.janettha.plchallenge
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.janettha.plchallenge.batteryIndicatorUI.BatteryIndicatorUI2
 import com.janettha.plchallenge.ui.theme.PLChallengeTheme
 
 class MainActivity : ComponentActivity() {
-    @SuppressLint("NewApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PLChallengeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    BatteryIndicatorUI2(modifier = Modifier.padding(innerPadding))
+                    Text(modifier = Modifier.padding(innerPadding),
+                        text = "")
                 }
             }
         }
@@ -32,7 +31,8 @@ class MainActivity : ComponentActivity() {
 fun ThousandsSeparatorPickerPreview() {
     PLChallengeTheme {
         Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-            BatteryIndicatorUI2(modifier = Modifier.padding(innerPadding))
+            Text(modifier = Modifier.padding(innerPadding),
+                text = "hola")
         }
     }
 }
